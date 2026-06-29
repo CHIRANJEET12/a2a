51bc0de9a9b8f967490ab45ccba882af6f227335
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2026-06-29 13:03:43 +0530

    commit

--- debate_ai/src/config.py
+++ debate_ai/src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 class Settings(BaseSettings):
-    GROQ_API_KEY: str
+    # GROQ_API_KEY: str
     TRAVILY_API_KEY: str
 
     model_config = SettingsConfigDict(
